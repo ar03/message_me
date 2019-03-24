c675378b2277e00fc62d874fc0774b2afd2cb4e7
Geralt of Rivia <alex@AlexH.local>
2019-03-24 11:47:43 -0500

    Add message model and association

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+=begin
 User.create(username: "Alex0", password: "password")
 User.create(username: "Alex1", password: "password")
 User.create(username: "Alex2", password: "password")
 User.create(username: "Alex3", password: "password")
 User.create(username: "Alex4", password: "password")
 User.create(username: "Alex5", password: "password")
+=end
+message_list = [
+  [ "Lorem Ipsum ", 1 ],
+  [ "Orem Ipsum L", 2 ],
+  [ "Rem Ipsum Lo", 3 ],
+  [ "Em Ipsum Lor", 4 ],
+  [ "M Ipsum Lore", 5 ],
+  [ "Ipsum Lorem ", 6 ]
+]
+
+message_list.each do |body, user_id|
+  Message.create(body: body, user_id: user_id)
+end
