43f0d661797d84b315a78e2844a0883801bc781e
Geralt of Rivia <alex@AlexH.local>
2019-03-25 19:08:05 -0500

    dispaly messages using partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,17 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.message.build(message_params)
-    ActionCable.server.broadcast "chatroom_channel", foo: message.body
+    ActionCable.server.broadcast "chatroom_channel", mod_message: message_render(message)
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 
 end
